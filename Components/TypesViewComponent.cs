f62fcf61a9c16c902f005162ffd7961977f3ab95
dongkya <kdh2395478@gmail.com>
2022-11-06 00:52:59 -0600

    11-05 first

---
+++ Components/TypesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using IS405.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS405.Components
+{
+    public class TypesViewComponent:ViewComponent
+    {
+        private EmployeeDbContext _context { get; set; }
+        public TypesViewComponent(EmployeeDbContext temp)
+        {
+            _context = temp;
+        }
+        public IViewComponentResult Invoke()
+        {
+            //var types = _context.Employees.Select(x = )
+            return View();
+        }
+    }
+
+}
